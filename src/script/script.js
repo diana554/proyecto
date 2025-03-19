292593a50cb5ae45df6ddc62e0c19c131abe3f61
Diana <diazmes18@gmail.com>
2025-03-19 15:31:21 -0500

    inicio

--- src/script/script.js
+++ src/script/script.js
@@ -1,31 +1,6 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const proyectos = [
-        {
-            titulo: "informativas",
-            descripcion: "Paginas informativas",
-            imagen: "./src/images/paginasinfo.png"
-        },
-        {
-            titulo: "formularios",
-            descripcion: "Formularios",
-            imagen: "./src/images/formulario.png"
-        },
-        {
-            titulo: "buscador",
-            descripcion: "Buscador",
-            imagen: "./src/images/buqueda.png"
-        }
-    ];
+const menuToggle = document.getElementById("menu-toggle");
+    const menu = document.getElementById("menu");
 
-    const contenedor = document.getElementById("proyectos-container");
-
-    proyectos.forEach(proyecto => {
-        contenedor.innerHTML += `
-            <div class="p-4 bg-deepBlue rounded-lg text-light text-center">
-                <img src="${proyecto.imagen}" alt="${proyecto.titulo}" class="w-full h-40 object-cover rounded mb-2">
-                <h3 class="text-xl font-bold">${proyecto.titulo}</h3>
-                <p class="text-softGray">${proyecto.descripcion}</p>
-            </div>
-        `;
+    menuToggle.addEventListener("click", () => {
+        menu.classList.toggle("hidden");
     });
-});
